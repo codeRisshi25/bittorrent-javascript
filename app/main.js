ae3d3ad752a0573236718022f3522bf841cdaa41
Risshi25 <risshirajsen@gmail.com>
2024-09-17 14:25:33 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -21,7 +21,6 @@ function main() {
   const command = process.argv[2];
 
   // You can use print statements as follows for debugging, they'll be visible when running tests.
-  console.log("Logs from your program will appear here!");
   if (command === "decode") {
     const bencodedValue = process.argv[3];
   
