c91404f400b8d2af988687ca734286ca6a3414cd
Risshi25 <risshirajsen@gmail.com>
2024-09-17 15:12:34 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -4,6 +4,7 @@ const util = require("util");
 // Examples:
 // - decodeBencode("5:hello") -> "hello"
 // - decodeBencode("10:hello12345") -> "hello12345"
+// - decodeBencode("i12345e") -> 12345
 function decodeBencode(bencodedValue) {
   // Check if the first character is a digit
   if (!isNaN(bencodedValue[0])) {
@@ -12,6 +13,8 @@ function decodeBencode(bencodedValue) {
       throw new Error("Invalid encoded value");
     }
     return bencodedValue.substr(firstColonIndex + 1);
+  } else if (bencodedValue[0] === "i") {
+    return bencodedValue.substr(1, bencodedValue.length - 2);
   } else {
     throw new Error("Only strings are supported at the moment");
   }
@@ -19,13 +22,8 @@ function decodeBencode(bencodedValue) {
 
 function main() {
   const command = process.argv[2];
-
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
   if (command === "decode") {
     const bencodedValue = process.argv[3];
-  
-    // In JavaScript, there's no need to manually convert bytes to string for printing
-    // because JS doesn't distinguish between bytes and strings in the same way Python does.
     console.log(JSON.stringify(decodeBencode(bencodedValue)));
   } else {
     throw new Error(`Unknown command ${command}`);
